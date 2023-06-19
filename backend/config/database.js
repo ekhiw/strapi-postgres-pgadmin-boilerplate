563d9048319ea01e297ff4f5ae602f003512e8f6
Ekky Hermestian IW <ekkyhiw@gmail.com>
2023-06-20 01:21:33 +0700

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
-import path from 'path';
+const path = require('path');
 
-export default ({ env }) => {
+module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
@@ -75,8 +75,7 @@ export default ({ env }) => {
         filename: path.join(
           __dirname,
           '..',
-          '..',
-          env('DATABASE_FILENAME', 'data.db')
+          env('DATABASE_FILENAME', '.tmp/data.db')
         ),
       },
       useNullAsDefault: true,
